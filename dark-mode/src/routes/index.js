cd877536aeb1c65e6e62033249ef88b9ab74a3b6
tejash-9 <58841046+tejash-9@users.noreply.github.com>
2022-03-18 16:51:21 +0530

    Update index.js

--- dark-mode/src/routes/index.js
+++ dark-mode/src/routes/index.js
@@ -1,8 +1,4 @@
 import Home from './App';
 
-import React from 'react';
-import { ReactDOM } from 'react';
-import './styles/_dark-module.scss';
-import './routes/app/styles/_app.scss'
 
 export default Home;
