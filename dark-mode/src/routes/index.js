629cb3d3fc15152444560064b50c2b026504c821
tejash-9 <999tejashwi@gmail.com>
2022-03-18 07:40:39 +0530

    your message is commited

--- dark-mode/src/routes/index.js
+++ dark-mode/src/routes/index.js
@@ -1,5 +1,8 @@
 import Home from './App';
 
-// Use something like react-router-dom to manage multiple pages/routes
+import React from 'react';
+import { ReactDOM } from 'react';
+import './styles/_dark-module.scss';
+import './routes/app/styles/_app.scss'
 
 export default Home;
